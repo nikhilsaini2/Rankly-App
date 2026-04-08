a572f18ff3968d1e2a55d7069031847954ad392a
nikhilsaini2 <nikhilsaini7981@gmail.com>
2026-04-08 16:50:57 +0530

    fix atsscorescreen

--- src/screens/resume/ATSScoreScreen.tsx
+++ src/screens/resume/ATSScoreScreen.tsx
@@ -140,7 +140,6 @@ export default function AtsScoreScreen() {
         style={{ flex: 1 }}
         contentContainerStyle={{
           paddingHorizontal: 20,
-          paddingTop: insets.top,
           paddingBottom: bottomInset,
         }}
         showsVerticalScrollIndicator={false}
